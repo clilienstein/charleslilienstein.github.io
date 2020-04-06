0b5c0cbf5d9a614357e9461e4b7614e2da4c397a
clilienstein <60356495+clilienstein@users.noreply.github.com>
2020-04-06 12:20:39 -0400

    class work

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -2,6 +2,26 @@
     //all js goes in here
     console.log("we loaded")
 
-    $("#title").css("color", "#bada55")
-    $("#title").html("<p>More information #1</p>")
+    $("#button1").click(function() {
+      // code that runs when you click on the part of the page that has id=#button1
+      // $("#title").css("color", "#f9432a");
+      // $("#title").css("font-size", "500px")
+      $("#title").addClass("big-text");
+    });
+
+    $("#button2").click(function() {
+      // stuff we put here will happen when you click button2.
+      $("#title").removeClass("big-text");
+    })
+
+    $("p").mouseover(function() {
+      $("p").css("font-size", "21px")
+    });
+
+    $("p").mouseleave(function() {
+      $("p").css("background-color", "yellow");
+      $("p").css("font-size", "16px");
+    });
+    // $("#title").css("font-size", "200px")
+    // $("#title").html("More information #1")
   });
