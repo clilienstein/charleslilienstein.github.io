8ab46c8430477f04c0cccd223a7df2d3cc979e24
clilienstein <60356495+clilienstein@users.noreply.github.com>
2020-04-15 10:59:57 -0400

    before office hours

--- hw/jquery/js/main.js
+++ hw/jquery/js/main.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-      console.log("page laoded");
 
-      $("#button1").click(function() {
-          $("#title").css("color", "#f9432a");
-          $("#title").css("font-size", "500px");
-          $("#title").addClass("big-text");
-        }
-        // let titleClciked = false;
-        // $("title").click(function() {
-        //   if (titleClicked = false) {
-        //
-        //     $("title").html("Dope <em>Art</em> In My House");
-        //
-        //     titleClicked = true;
-        //   } else if (titleClicked = true) {
-        //
-        //     $("title").html("Dope <em>Art</em> In My House");
-        //
-        //     titleClicked = false;
-        //   }
-        //
-        //   // $("#title").click(function() {
-        //   //   $("#title").html("<em>Art</em> In My House");
-        //   // });
-        //   $("body").css("background-color", "#____");
-        //   $("html").css("font-family", "Lacquer, cursive");
-        //
+  $("#button1").click(function() {
+    $("#title").css("color", "#f9432a");
+    $("#title").css("text-decoration", "line-through");
+  });
 
-      });
+
+  // let titleClciked = false;
+  // $("title").click(function() {
+  //   if (titleClicked = false) {
+  //
+  //     $("title").html("Dope <em>Art</em> In My House");
+  //
+  //     titleClicked = true;
+  //   } else if (titleClicked = true) {
+  //
+  //     $("title").html("Dope <em>Art</em> In My House");
+  //
+  //     titleClicked = false;
+  //   }
+  //
+  //   // $("#title").click(function() {
+  //   //   $("#title").html("<em>Art</em> In My House");
+  //   // });
+  //   $("body").css("background-color", "#____");
+  //   $("html").css("font-family", "Lacquer, cursive");
+  //
+
+});
