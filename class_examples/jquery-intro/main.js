515053f0df6eb906679c0646063c364873681041
clilienstein <60356495+clilienstein@users.noreply.github.com>
2020-04-15 13:56:36 -0400

    final website edit

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -20,5 +20,36 @@
     // });
     // $("#title").css("font-size", "200px")
     // $("#title").html("More information #1")
+
+
+    // $("title1").mouseover(function() {
+    //   $("title1").css("transform", "scale(1.1, 1.1)");
+    // }, function() {
+    //   $(this).css("transform", "none");
+    // });
+
+
+    // EVERYTHING BELOW IS FROM CLASS
+    // let titleClciked = false;
+    // $("title").click(function() {
+    //   if (titleClicked = false) {
+    //
+    //     $("title").html("<em>Art</em> In My House");
+    //
+    //     titleClicked = true;
+    //   } else if (titleClicked = true) {
+    //
+    //     $("title").html("<em>Art</em> In My House");
+    //
+    //     titleClicked = false;
+    //   }
+    //
+    //   // $("#title").click(function() {
+    //   //   $("#title").html("<em>Art</em> In My House");
+    //   // });
+    //   $("body").css("background-color", "#____");
+    //   $("html").css("font-family", "Lacquer, cursive");
+    //
+
     ?
   });
--- hw/jquery/js/main.js
+++ hw/jquery/js/main.js
@@ -21,54 +21,27 @@ $(document).ready(function() {
   });
 
   $("#title").click(function() {
-    $("#title").html("");
+    $("#title").html("Everything is Art");
   });
 
   $("#art1").mouseover(function() {
     $("#art1").css("width", "150%");
   });
 
+  $("#art2").mouseover(function() {
+    $("#art2").css("width", "150%");
+  });
 
+  $("#art3").mouseover(function() {
+    $("#art3").css("width", "150%");
+  });
 
+  $("#art4").mouseover(function() {
+    $("#art4").css("width", "150%");
+  });
 
-
-
-
-
-  // // trying to make the img bigger when mouseover
-  // $("title1").mouseover(function() {
-  //     $("title1").css(");
-  //     });
-
-
-
-  // $("title1").mouseover(function() {
-  //   $("title1").css("transform", "scale(1.1, 1.1)");
-  // }, function() {
-  //   $(this).css("transform", "none");
-  // });
-
-
-  // EVERYTHING BELOW IS FROM CLASS
-  // let titleClciked = false;
-  // $("title").click(function() {
-  //   if (titleClicked = false) {
-  //
-  //     $("title").html("<em>Art</em> In My House");
-  //
-  //     titleClicked = true;
-  //   } else if (titleClicked = true) {
-  //
-  //     $("title").html("<em>Art</em> In My House");
-  //
-  //     titleClicked = false;
-  //   }
-  //
-  //   // $("#title").click(function() {
-  //   //   $("#title").html("<em>Art</em> In My House");
-  //   // });
-  //   $("body").css("background-color", "#____");
-  //   $("html").css("font-family", "Lacquer, cursive");
-  //
+  $("#art5").mouseover(function() {
+    $("#art5").css("width", "150%");
+  });
 
 });
