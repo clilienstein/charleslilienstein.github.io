06483186d7c4a1bedf0c6ec8a38bf0d9e63235c1
clilienstein <60356495+clilienstein@users.noreply.github.com>
2020-05-05 00:42:38 -0400

    work

---
+++ hw/finalproject/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $("#title").click(function() {
+    $("#title").html("Cannabis is for everyone!");
+  });
+
+  $("#button1").click(function() {
+    $("#list").hide();
+  });
+
+  $("#button2").click(function() {
+    $("#list").show();
+  });
+
+
+
+});
